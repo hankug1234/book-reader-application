b9c0514cc32cfb74fb0adcc590f9f624bd7b763a
kug han <hankug12@wisenut.co.kr>
2024-10-08 15:55:04 +0900

    animation effect append

--- src/app/components/test/cardTest.js
+++ src/app/components/test/cardTest.js
@@ -1,8 +1,25 @@
 import ResizableCard from "../../utils/components/resizableCard/resizableCard";
+import "./css/textSlider.css"
 
 const CardTest = () => {
 
-    return <ResizableCard width={300} height={500}/>
+    return (
+        <>
+        <div class={"test-div"}>
+            <ResizableCard width={300} height={500}>
+                    <h2>Text that will slide in from the left</h2>
+                    <div className={"vertical-line-left"}></div>
+                    <div className={"vertical-line-right"}></div>
+                    <div className={"horizontal-line-top"}></div>
+                    <div className={"horizontal-line-bottom"}></div>
+                    <div className={"diagonal-line-top-left"}></div>
+                    <div className={"diagonal-line-top-right"}></div>
+                    <div className={"diagonal-line-bottom-left"}></div>
+                    <div className={"diagonal-line-bottom-right"}></div>
+            </ResizableCard>
+        </div>
+        </>
+    )
 }
 
 export default CardTest
