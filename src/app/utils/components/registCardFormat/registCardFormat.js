e205578a086e6bd9e473dab7f77ad05d93dcad12
kug han <hankug12@wisenut.co.kr>
2024-10-28 14:34:58 +0900

    update

--- src/app/utils/components/registCardFormat/registCardFormat.js
+++ src/app/utils/components/registCardFormat/registCardFormat.js
@@ -4,7 +4,7 @@ const InfoList =  ({infos}) => {
     return (
         <>
         {
-            infos.map((info,i)=><h1 key={i} style={{bottom:`${70 - 7*i}%`}} className={"dataInfo_r nanum-brush-script-regular"}>{info}</h1>)
+            infos.map((info,i)=><h1 key={i} style={{bottom:`${70 - 10*i}%`}} className={"dataInfo_r nanum-brush-script-regular"}>{info}</h1>)
         }
         </>
     )
@@ -21,7 +21,6 @@ const RegistedCardFormat = ({registedData,type,width,height}) => {
             <h2 className={"dataType_r nanum-brush-script-regular"}>{`${type} DATA`}</h2>
             <div className={"dataBody"}>
                 <InfoList infos={Object.entries(registedData).map((items)=>`${items[0]} : ${items[1]}`).filter((s)=>infoFilter(s)===true)}/>
-                <label for={"dataDescription_r"} className={"descriptionLabel_r nanum-brush-script-regular"}>{"description:"}</label>
                 <div id={"dataDescription_r"} className={"dataDescription_r nanum-brush-script-regular"}>{registedData.description}</div>
             </div>
             <h2 className={"dataName_r nanum-brush-script-regular"}>{registedData.data_set_name}</h2>
